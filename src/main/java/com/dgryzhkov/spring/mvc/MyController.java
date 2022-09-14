49ae370b2c9eff5a08020cad2ba370fc5194721c
Denis Ryzhkov <dgryzhkov@gmail.com>
2022-09-14 12:47:16 +0700

    get post method

--- src/main/java/com/dgryzhkov/spring/mvc/MyController.java
+++ src/main/java/com/dgryzhkov/spring/mvc/MyController.java
@@ -3,8 +3,10 @@ package com.dgryzhkov.spring.mvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 import javax.validation.Valid;
 
@@ -27,7 +29,7 @@ public class MyController {
         return "ask-emp-details-view";
     }
 
-    @RequestMapping("/showDetails")
+    @GetMapping("/showDetails")
     public String showEmpDetails(@Valid @ModelAttribute("employee") Employee emp
             , BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
